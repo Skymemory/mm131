5222822daddea20e69cf27c016245328395b58fb
Memory Sky <memorysky@SkyMemory.local>
2019-05-21 22:19:38 +0800

    Add mm131.py and document.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # mm131
-Batch pictures downloader from mm131
+Batch picture downloader for mm131
+
+#### 依赖
+
+Python >= 3.7
+
+
+#### 实现原理
+
+mm131网站上的一张图片由一个二元组标识（一级索引，二级索引），其中二级索引的范围是可以获取到的
+
+比如某个分类下妹子的图片地址为:[http://www.mm131.com/xinggan/4100.html](http://www.mm131.com/xinggan/4100.html)，其中4100就是一级索引编号，在该页面解析html获取到总共页数也就间接知道了二级索引的范围，这样就能批量的下载图片啦
+
+#### 使用方式
+
+mm131.py提供了几个选项说明，通过`python test.py -h`可参看:
+
+```shell
+usage: mm131.py [-h] [-c CATEGORY] [-r RANGE] [-d DIRECTORY] [--level LEVEL]
+
+Picture downloader for mm131
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -c CATEGORY    Specify which category to download(default xinggan)
+  -r RANGE       Specify first index,format: start[,end]
+  -d DIRECTORY   Save directory(default /tmp)
+  --level LEVEL  Logging level
+```
+
+
+
+- `-c`：指定下载图片的分类，默认xinggan
+- `-r`：指定一级索引范围
+- `-d`：指定保存目录，默认/tmp目录
+- `—level`：日志打印级别
+
+比如下载一级索引为4100图片:
+
+```shell
+> python mm131.py -r 4100
+INFO:root:-----------------statistics------------------
+INFO:root:Success:65		Fail:0
+INFO:root:Cost time:1.678253173828125
+```
+
---
+++ mm131.py
@@ -0,0 +1,118 @@
+import argparse
+import asyncio
+import itertools
+import logging
+import os
+import re
+import time
+
+import aiohttp
+from bs4 import BeautifulSoup
+
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36'
+
+
+async def fetch_and_save(session, url, headers, path):
+    logging.debug('Downloading {}...'.format(url))
+    resp = await session.get(url, allow_redirects=False, headers=headers)
+    content = await resp.content.read()
+    with open(path, 'wb') as out:
+        out.write(content)
+
+
+async def crawler(session, base_url, category, index, save_dir):
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+    success, fail = 0, 0
+    resp = await session.get(base_url, allow_redirects=False)
+    # ignore unexpected error
+    if resp.status != 200:
+        logging.warning('{} got status {}, ignore it.'.format(base_url, resp.status))
+        return 0, 0
+
+    html = await resp.content.read()
+    text = BeautifulSoup(html, 'html.parser').select_one('.content-page > span').get_text()
+    pages = int(re.findall(r'\d+', text)[0])
+
+    tasks = []
+    urls = []
+    for i in range(1, pages + 1):
+        path = os.path.join(save_dir, '{}.jpg'.format(i))
+        pic_url = 'http://img1.mm131.me/pic/{}/{}.jpg'.format(index, i)
+        headers = {
+            'User-Agent': DEFAULT_USER_AGENT,
+            'Referer': 'http://www.mm131.com/{}/{}{}.html'.format(category, index, "1" if i == 1 else "_{}".format(i))
+        }
+
+        urls.append(pic_url)
+        tasks.append(fetch_and_save(session, pic_url, headers, path))
+
+    rs = await asyncio.gather(*tasks, return_exceptions=True)
+
+    for pic_url, result in zip(urls, rs):
+        if isinstance(result, Exception):
+            logging.error('{} got unexpected error {}'.format(pic_url, result))
+            fail += 1
+        else:
+            success += 1
+
+    return success, fail
+
+
+async def download(category, start, end, save_dir):
+    start_at = time.time()
+    success, fail = 0, 0
+    it = iter(range(start, end))
+    async with aiohttp.ClientSession() as session:
+        while 1:
+            chunk = tuple(itertools.islice(it, 200))
+            if not chunk:
+                break
+
+            urls = []
+            tasks = []
+            for elem in chunk:
+                base_url = 'http://www.mm131.com/{}/{}.html'.format(category, elem)
+                folder = os.path.join(save_dir, str(elem))
+
+                urls.append(base_url)
+                tasks.append(crawler(session, base_url, category, elem, folder))
+
+            rs = await asyncio.gather(*tasks, return_exceptions=True)
+            for base_url, result in zip(urls, rs):
+                if isinstance(result, Exception):
+                    logging.error('{} got unexpected error {}'.format(base_url, result))
+                else:
+                    s, f = result
+                    success += s
+                    fail += f
+
+    logging.info('-' * 20 + 'Statistics' + '-' * 20)
+    logging.info('Success:{}\t\tFail:{}'.format(success, fail))
+    logging.info('Cost time:{}'.format(time.time() - start_at))
+
+
+def main():
+    args = ARGS.parse_args()
+    rr = list(map(int, args.r.split(',')))
+    if len(rr) == 1:
+        start = end = rr[0]
+    else:
+        start, end = rr
+
+    logging.getLogger().setLevel(args.level)
+
+    asyncio.run(download(args.c, start, end + 1, args.d))
+
+
+ARGS = argparse.ArgumentParser(description='Picture downloader for mm131')
+ARGS.add_argument('-c', metavar='CATEGORY',
+                  choices=['xinggan', 'qingchun', 'xiaohua', 'chemo', 'qipao', 'mingxing'],
+                  default='xinggan', help='Specify which category to download(default xinggan)')
+ARGS.add_argument('-r', metavar='RANGE', help='Specify first index,format: start[,end]')
+ARGS.add_argument('-d', metavar='DIRECTORY', help='Save directory(default /tmp)', default='/tmp')
+ARGS.add_argument('--level', help='Logging level', default=logging.INFO, type=int)
+
+if __name__ == '__main__':
+    main()
